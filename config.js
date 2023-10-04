dff8e0cd0795c5e35b5e4db40d9da1217e42cf63
henriquelspinto <1211201@isep.ipp.pt>
2023-10-04 18:12:10 +0100

    fix: config.js db name altered to a more convenient one

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ export default {
   /**
    * That long string from mlab
    */
-  databaseURL: process.env.MONGODB_URI || "mongodb://localhost:27017/test",
+  databaseURL: process.env.MONGODB_URI || "mongodb://localhost:27017/nodejs_base",
 
   /**
    * Your secret sauce
